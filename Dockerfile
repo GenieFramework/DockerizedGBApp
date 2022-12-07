b8bc05e4f41b8f8d25ccf2dfa3de1b3ff288f2f1
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2022-12-07 22:21:07 +0530

    Fixed dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
-# pull latest julia image
 FROM julia:latest
 WORKDIR /app
 COPY . .
 EXPOSE 8000
 
+RUN julia --project -e 'using Pkg; Pkg.instantiate(); Pkg.precompile();'
+
 ENV GENIE_HOST "0.0.0.0"
 ENV PORT "9001"
-ENV WSPORT "8000"
+ENV WSPORT "9001"
 
-# ENTRYPOINT [“julia”]
-# RUN julia test.jl
 CMD ["julia", "startup.jl"]
--- startup.jl
+++ startup.jl
@@ -1,9 +1,6 @@
-# installing dependencies REQUIREMENT.TXT
-using Pkg; 
+using Pkg;
 Pkg.activate(".");
-Pkg.instantiate();
-# import tensorflow.tf as tf
-using GenieFramework; # precompilation
+using GenieFramework;
 using GenieFramework.Genie;
 Core.eval(Main, :(const UserApp = $(@__MODULE__)));
 Genie.genie(context = @__MODULE__);
