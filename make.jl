e1a3a68cf0a2ad4e65586000b95e046f10bc5f93
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2022-12-21 11:12:41 +0530

    producing .so packagecompiler

--- make.jl
+++ make.jl
@@ -1,10 +1,10 @@
 using PackageCompiler
 
-include("compiled/packages.jl")
+include("packages.jl")
 
 PackageCompiler.create_sysimage(
   PACKAGES,
-  sysimage_path = "compiled/sysimg.so",
-  precompile_execution_file = "compiled/precompile.jl",
+  sysimage_path = "sysimg.so",
+  precompile_execution_file = "precompile.jl",
   cpu_target = PackageCompiler.default_app_cpu_target()
 )
